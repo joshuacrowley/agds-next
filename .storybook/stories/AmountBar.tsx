6e45d24596528a3a20e4bedb4cc8d01671d7bed2
Joshua Crowley <joshua.crowley@gmail.com>
2022-09-30 13:58:13 +1000

    Update AmountBar.tsx

--- .storybook/stories/AmountBar.tsx
+++ .storybook/stories/AmountBar.tsx
@@ -17,106 +17,139 @@ import { Modal } from '@ag.ds-next/modal';
 import { Textarea } from '@ag.ds-next/textarea';
 import { Select } from '@ag.ds-next/select';
 import { useTernaryState } from '@ag.ds-next/core';
+import { QuotaToken } from './QuotaToken';
 
 export default {
 	title: 'Quota/AmountBars',
 };
 
-export const Basic = () => {
-	const [isModalOpen, openModal, closeModal] = useTernaryState(false);
+//https://www.magicpattern.design/tools/css-backgrounds
+
+const colours = {
+	success: '#0B996C',
+	successShade: '#046F4D',
+	warning: '#F69900',
+	altShade: '#E0E0E0',
+};
+
+const Diagonal = {
+	'background-color': 'rgba(159, 159, 171, 0.12)',
+	opacity: '0.5',
+	'background-size': '7px 7px',
+	'background-image':
+		'repeating-linear-gradient(45deg, #000000 0, #000000 0.7000000000000001px, rgba(159, 159, 171, 0.12) 0, rgba(159, 159, 171, 0.12) 50%)',
+};
+
+const Dots = {
+	'background-color': 'rgba(159, 159, 171, 0.12)',
+	opacity: 0.5,
+	'background-image':
+		'radial-gradient(#000000 0.9px, rgba(159, 159, 171, 0.12) 0.9px)',
+	'background-size': '18px 18px',
+};
+
+const Moon = {
+	'background-color': 'rgba(159, 159, 171, 0.12)',
+	opacity: 0.4,
+	'background-image':
+		'radial-gradient( ellipse farthest-corner at 8px 8px , #000000, #000000 50%, rgba(159, 159, 171, 0.12) 50%)',
+	'background-size': '8px 8px',
+};
+
+const LABEL_COLOUR_TEXTURE = {
+	assigned: { colour: colours.success, texture: Moon },
+	assignedAlt: { colour: colours.successShade, texture: Dots },
+	uncommited: { colour: colours.warning, texture: Diagonal },
+	closed: { colour: colours.altShade, texture: Moon },
+};
+
+const Bar = ({ data, height }) => {
+	console.log(data);
+	return (
+		<Flex
+			justifyContent={'flex-start'}
+			alignItems={'stretch'}
+			flexDirection={'row'}
+			flexGrow={1}
+			width={'100%'}
+			height={`${height}px`}
+			background={'shade'}
+		>
+			{data.map((bar) => {
+				let texture = LABEL_COLOUR_TEXTURE[bar.label].texture;
+				let colour = LABEL_COLOUR_TEXTURE[bar.label].colour;
+				return (
+					<div
+						style={{
+							...texture,
+							backgroundColor: colour,
+							flexBasis: `${bar.amount}%`,
+						}}
+					></div>
+				);
+			})}
+		</Flex>
+	);
+};
+
+const AmountBars = ({ data, total, unit, quota, context }) => {
+	let percentages = data.map((item) => {
+		return { amount: (item.amount / total) * (100 / 1), label: item.label };
+	});
 
 	return (
 		<div>
-			<Flex>
-				<Box background={'success'} color={'success'} width={'12%'}>
-					J
-				</Box>
-			</Flex>
-			<Button onClick={openModal}>Cancel Certificate</Button>
-			<Modal
-				isOpen={isModalOpen}
-				onDismiss={closeModal}
-				title="Cancel Certificate"
-				actions={
-					<ButtonGroup>
-						<Button onClick={closeModal}>Proceed with Cancelation</Button>
-						<Button variant="secondary" onClick={closeModal}>
-							Dismiss
-						</Button>
-					</ButtonGroup>
-				}
+			<Box
+				padding={1}
+				display="block"
+				border
+				borderColor="muted"
+				background={'body'}
+				rounded
 			>
-				<Stack gap={1} paddingTop={1}>
-					<Select
-						label="Reason from Exporter"
-						onBlur={() => {}}
-						onChange={function noRefCheck() {}}
-						onFocus={function noRefCheck() {}}
-						block
-						options={[
-							{
-								label: 'Exporter requested change',
-								value: 'a',
-							},
-							{
-								label: 'Exporter requested cancel',
-								value: 'b',
-							},
-							{
-								label: 'Importing country requested cancel',
-								value: 'c',
-							},
-							{
-								label: 'Incorrect exporter data',
-								value: 'd',
-							},
-							{
-								label: 'Incorrect certificate data',
-								value: 'e',
-							},
-							{
-								label: 'Billing error',
-								value: 'f',
-							},
-							{
-								label: 'Billing error',
-								value: 'Quota Admin reason',
-							},
-						]}
-						placeholder="Please select"
-						required
-					/>
-
-					<Select
-						block
-						label="Balance adjustment"
-						onBlur={() => {}}
-						onChange={function noRefCheck() {}}
-						onFocus={function noRefCheck() {}}
-						options={[
-							{
-								label: 'Pending',
-								value: 'a',
-							},
-							{
-								label: 'Immediate balance adjustment',
-								value: 'b',
-							},
-						]}
-						placeholder="Please select"
-						required
-					/>
-
-					<Textarea
-						label="Comment"
-						onBlur={() => {}}
-						onChange={function noRefCheck() {}}
-						onFocus={function noRefCheck() {}}
-						required={false}
-						block
-					/>
+				<Stack gap={0.5} alignItems={'flex-start'}>
+					<Flex>
+						<QuotaToken shortHand {...quota} />
+					</Flex>
+					<Bar height={context === 'bucket' ? 24 : 48} data={percentages}></Bar>
+					<Flex
+						flexDirection={'row'}
+						width={'100%'}
+						justifyContent={'space-between'}
+					>
+						<Flex flexDirection={'row'} gap={1}>
+							{data.map((item) => (
+								<Text>
+									{item.amount.toLocaleString()} {unit} {item.label}
+								</Text>
+							))}
+						</Flex>
+						<Text>
+							Total available : {total.toLocaleString()} {unit}
+						</Text>
+					</Flex>
 				</Stack>
-			</Modal>
+			</Box>
 		</div>
 	);
 };
+
+const exampleQuota = {
+	market: 'EU',
+	quotaCode: 'BUFFM',
+	periodEnd: '23',
+	agreementCode: 'FTA',
+};
+
+export const BucketStandard = () => (
+	<AmountBars
+		data={[
+			{ amount: 50000, label: 'assigned' },
+			{ amount: 50000, label: 'uncommited' },
+		]}
+		context={'bucket'}
+		quota={exampleQuota}
+		total={100000}
+		unit={'kgs'}
+	/>
+);
